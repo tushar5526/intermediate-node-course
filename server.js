aa82fb781b07fb8a6d3408d20744faabc180299a
Tushar <codingid6@gmail.com>
2020-06-14 17:03:35 +0530

    s

--- server.js
+++ server.js
@@ -6,19 +6,54 @@ const app= express();
 
 app.use(bodyParser.json());
 
+const User=require('./models/User');
+mongoose.connect('mongodb://localhost/userData')
+
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
+  User.create(
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    (err,data)=>{
+    if (err){
+      res.json({success: false,message: err})
+    } else if (!data){
+      res.json({success: false,message: "Not Found"})
+    } else {
+      res.json({success: true,data: data})
+    }
+  })
 })
+
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  // User.findById()
+  User.findById(req.params.id,(err,data)=>{
+    if (err){
+      res.json({
+        success: false,
+        message: err
+      })
+    } else if (!data){
+      res.json({
+        success: false,
+        message: "Not Found"
+      })
+    } else {
+      res.json({
+        success: true,
+        data: data
+      })
+    }
+  })
 })
 // UPDATE
 .put((req,res)=>{
